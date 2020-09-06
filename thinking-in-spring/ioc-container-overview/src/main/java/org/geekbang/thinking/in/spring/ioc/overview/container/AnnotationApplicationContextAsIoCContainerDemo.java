1a0ba14300002ab96b275e204aa74cf92501a5f6
zw <1094660626@qq.com>
2020-09-06 18:24:55 +0800

    基于xml或注解，创建IoC容器并获取Bean

--- thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/container/AnnotationApplicationContextAsIoCContainerDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/container/AnnotationApplicationContextAsIoCContainerDemo.java
@@ -29,7 +29,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Map;
 
 /**
- * 注解能力 {@link ApplicationContext} 作为 IoC 容器示例
+ *  {@link ApplicationContext} 注解：作为 IoC 容器示例
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since
@@ -38,12 +38,16 @@ import java.util.Map;
 public class AnnotationApplicationContextAsIoCContainerDemo {
 
     public static void main(String[] args) {
+
         // 创建 BeanFactory 容器
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
         // 将当前类 AnnotationApplicationContextAsIoCContainerDemo 作为配置类（Configuration Class）
         applicationContext.register(AnnotationApplicationContextAsIoCContainerDemo.class);
+
         // 启动应用上下文
         applicationContext.refresh();
+
         // 依赖查找集合对象
         lookupCollectionByType(applicationContext);
 
@@ -59,7 +63,7 @@ public class AnnotationApplicationContextAsIoCContainerDemo {
     public User user() {
         User user = new User();
         user.setId(1L);
-        user.setName("小马哥");
+        user.setName("小马哥AAA");
         return user;
     }
 
